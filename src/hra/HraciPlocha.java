ac952dd02943d6b36287daea12c4e23ab706c09a
Zarcos <zarcos@seznam.cz>
2016-11-02 13:08:35 +0100

    konec 1. hodiny - zacatek HraciPlochy

--- src/hra/HraciPlocha.java
+++ src/hra/HraciPlocha.java
@@ -1,7 +1,10 @@
 package hra;
 
-public class HraciPlocha {
+import javax.swing.JPanel;
+
+public class HraciPlocha extends JPanel {
 	public static final int VYSKA = 800;
 	public static final int SIRKA = 600;
+	
 	
 }
